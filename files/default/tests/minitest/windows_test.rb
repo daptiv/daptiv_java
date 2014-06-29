13315dc6fdad218221d9cc4625c03becc5709dc2
Shawn Neal <sneal@sneal.net>
2014-06-29 13:24:01 -0700

    fixup minitests to use attribute

--- files/default/tests/minitest/windows_test.rb
+++ files/default/tests/minitest/windows_test.rb
@@ -1,9 +1,9 @@
 class TestWindowsJava < MiniTest::Chef::TestCase
   def test_java_home_set_for_current_process
-    assert ENV['JAVA_HOME'] == node['java']['home']
+    assert ENV['JAVA_HOME'] == node['java']['java_home']
   end
 
   def test_java_exe_exists
-    assert File.exists?(File.join(node['java']['home'], 'bin', 'java.exe'))
+    assert File.exists?(File.join(node['java']['java_home'], 'bin', 'java.exe'))
   end
 end
