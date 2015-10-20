d6963b5ff3631aa2626e273b3af3a0f1a9aa0808
Shawn Neal <sneal@sneal.net>
2015-10-20 13:52:10 -0700

    Updated gems, Vagrant config, ChefSpec

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      ''
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
-version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.0.1').chomp
+version ((IO.read(ver_path) if File.exists?(ver_path)) || '1.0.0').chomp
 supports         'ubuntu'
 supports         'windows'
 depends          'apt'
