50001cb42601216c8a578cc3e8c867a3ba2fbd57
heathsnow <heath.snow@changepoint.com>
2016-11-29 10:40:42 -0800

    new build flow for ec2

--- spec/spec_helper_linux.rb
+++ spec/spec_helper_linux.rb
@@ -1,10 +1,12 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+ChefSpec::Coverage.start!
+
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
   config.platform = 'ubuntu'
-  config.version = '12.04'
+  config.version = '14.04'
 end
